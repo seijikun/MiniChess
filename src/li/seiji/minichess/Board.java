676255d166720ccba57f1e3c7fb692b1f22c93d1
Jonathan Ponzer <jonathan.ponzer@gmx.de>
2017-05-15 15:48:21 +0200

    Implemented symmScan() and moveList() for Move Algorithm

--- src/li/seiji/minichess/Board.java
+++ src/li/seiji/minichess/Board.java
@@ -1,9 +1,5 @@
 package li.seiji.minichess;
 
-import li.seiji.minichess.figure.*;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Board {
 
     /* BOARD DEFINITIONS */
@@ -15,45 +11,5 @@ public class Board {
             ".....\n" +
             "PPPPP\n" +
             "RNBQK";
-
-
-    /**
-     * Get a list of all possible moves for the given state.
-     * @param state Current state for which to get all possible moves.
-     * @return List of all possible moves for the current player
-     */
-    public static List<Move> getPossibleMoves(State state) {
-        List<Move> moves = new ArrayList<Move>();
-
-        for(int x = 0; x < state.board.length; ++x) {
-            for(int y = 0; y < state.board[x].length; ++y) {
-                char identifier = Character.toLowerCase(state.board[x][y]);
-                if(Player.parseIdentifier(state.board[x][y]) == state.turn) {
-                    switch(identifier) {
-                        case Pawn.identifier:
-                            Pawn.getPossibleMoves(state, new Square(x, y), state.turn, moves);
-                            break;
-                        case Rook.identifier:
-                            Rook.getPossibleMoves(state, new Square(x, y), state.turn, moves);
-                            break;
-                        case Bishop.identifier:
-                            Bishop.getPossibleMoves(state, new Square(x, y), state.turn, moves);
-                            break;
-                        case Knight.identifier:
-                            Knight.getPossibleMoves(state, new Square(x, y), state.turn, moves);
-                            break;
-                        case King.identifier:
-                            King.getPossibleMoves(state, new Square(x, y), state.turn, moves);
-                            break;
-                        case Queen.identifier:
-                            Queen.getPossibleMoves(state, new Square(x, y), state.turn, moves);
-                            break;
-                    }
-                }
-            }
-        }
-
-        return moves;
-    }
 
 }
---
+++ src/li/seiji/minichess/figure/Capture.java
@@ -0,0 +1,5 @@
+package li.seiji.minichess.figure;
+
+public enum Capture {
+    TRUE, FALSE, ONLY
+}
