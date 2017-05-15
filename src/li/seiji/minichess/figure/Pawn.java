ff3c4ea3462918bc3f3d1a5833cfdd2244487306
Jonathan Ponzer <jonathan.ponzer@gmx.de>
2017-05-15 13:33:40 +0200

    Implemented Pawn.getPossibleMoves();

--- src/li/seiji/minichess/figure/Pawn.java
+++ src/li/seiji/minichess/figure/Pawn.java
@@ -1,9 +1,6 @@
 package li.seiji.minichess.figure;
 
-import li.seiji.minichess.Move;
-import li.seiji.minichess.Player;
-import li.seiji.minichess.Square;
-import li.seiji.minichess.State;
+import li.seiji.minichess.*;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.List;
@@ -13,7 +10,22 @@ public class Pawn implements IFigure {
     public static final char identifier = 'p';
 
     public static void getPossibleMoves(State state, Square from, Player player, List<Move> result) {
-        throw new NotImplementedException(); //TODO: implement
+        if(player == Player.BLACK) {
+            if(from.x > 0 && state.board[from.y+1][from.x-1] != '.')
+                result.add(new Move(from, new Square(from.y+1, from.x-1)));
+            else if(from.x < Board.COLUMNS && state.board[from.y+1][from.x+1] != '.')
+                result.add(new Move(from, new Square(from.y+1, from.x+1)));
+            else
+                result.add(new Move(from, new Square(from.x, from.y+1)));
+        }
+        else {
+            if(from.x > 0 && state.board[from.y-1][from.x-1] != '.')
+                result.add(new Move(from, new Square(from.y-1, from.x-1)));
+            else if(from.x < Board.COLUMNS && state.board[from.y-1][from.x+1] != '.')
+                result.add(new Move(from, new Square(from.y-1, from.x+1)));
+            else
+                result.add(new Move(from, new Square(from.x, from.y-1)));
+        }
     }
 
 }
