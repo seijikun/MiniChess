cbd2e31ccd7e8b419cf5b7033f57ddb5f55b5e2c
Jonathan Ponzer <jonathan.ponzer@gmx.de>
2017-05-15 13:46:03 +0200

    Implemented King.getPossibleMoves();

--- src/li/seiji/minichess/figure/King.java
+++ src/li/seiji/minichess/figure/King.java
@@ -1,9 +1,6 @@
 package li.seiji.minichess.figure;
 
-import li.seiji.minichess.Move;
-import li.seiji.minichess.Player;
-import li.seiji.minichess.Square;
-import li.seiji.minichess.State;
+import li.seiji.minichess.*;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.List;
@@ -13,7 +10,22 @@ public class King implements IFigure {
     public static final char identifier = 'k';
 
     public static void getPossibleMoves(State state, Square from, Player player, List<Move> result) {
-        throw new NotImplementedException(); //TODO: implement
+        if(from.x > 0)
+            result.add(new Move(from, new Square(from.x-1, from.y)));
+        if(from.x < Board.COLUMNS)
+            result.add(new Move(from, new Square(from.x+1, from.y)));
+        if(from.y > 0)
+            result.add(new Move(from, new Square(from.x, from.y-1)));
+        if(from.y < Board.ROWS)
+            result.add(new Move(from, new Square(from.x, from.y+1)));
+        if(from.x > 0 && from.y > 0)
+            result.add(new Move(from, new Square(from.x-1, from.y-1)));
+        if(from.x > 0 && from.y < Board.ROWS)
+            result.add(new Move(from, new Square(from.x-1, from.y+1)));
+        if(from.x < Board.COLUMNS && from.y < Board.ROWS)
+            result.add(new Move(from, new Square(from.x+1, from.y+1)));
+        if(from.x < Board.COLUMNS && from.y > 0)
+            result.add(new Move(from, new Square(from.x-1, from.y+1)));
     }
 
 }
