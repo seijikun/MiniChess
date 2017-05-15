74e7dbfa893914f68f977b537860f4c577ede758
Markus Ebner <seiji@seiji.li>
2017-05-15 22:05:40 +0200

    Implement Move TestCase

--- test/li/seiji/minichess/MoveTest.java
+++ test/li/seiji/minichess/MoveTest.java
@@ -1,7 +1,27 @@
 package li.seiji.minichess;
 
-/**
- * Created by seiji on 5/15/17.
- */
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class MoveTest {
+
+    @Test
+    public void testMoveStringParsing() {
+        Move move = new Move("c2-e4");
+        assertSquareCorrect(move.from, 2, 1);
+        assertSquareCorrect(move.to, 4, 3);
+
+        move = new Move("e4-c2");
+        assertSquareCorrect(move.from, 4, 3);
+        assertSquareCorrect(move.to, 2, 1);
+    }
+
+
+
+    private static void assertSquareCorrect(Square square, int x, int y) {
+        assertEquals(x, square.x);
+        assertEquals(y, square.y);
+    }
+
 }
