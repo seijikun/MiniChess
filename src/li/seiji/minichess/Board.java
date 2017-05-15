1d63229ae9c9323a033207da259d1beaeb716536
Markus Ebner <seiji@seiji.li>
2017-05-15 12:31:22 +0200

    Implement the generation of possible moves in static Board-class

--- src/li/seiji/minichess/Board.java
+++ src/li/seiji/minichess/Board.java
@@ -1,14 +1,46 @@
 package li.seiji.minichess;
 
 
+import li.seiji.minichess.figure.*;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Board {
 
     public static List<Move> getPossibleMoves(State state) {
-        throw new NotImplementedException(); //TODO: implement
+        List<Move> moves = new ArrayList<Move>();
+
+        for(int x = 0; x < state.board.length; ++x) {
+            for(int y = 0; y < state.board[x].length; ++y) {
+                char identifier = Character.toLowerCase(state.board[x][y]);
+                Player player = Player.parseIdentifier(state.board[x][y]);
+
+                switch(identifier) {
+                    case Pawn.identifier:
+                        Pawn.getPossibleMoves(state, new Square(x, y), moves);
+                        break;
+                    case Rook.identifier:
+                        Rook.getPossibleMoves(state, new Square(x, y), moves);
+                        break;
+                    case Bishop.identifier:
+                        Bishop.getPossibleMoves(state, new Square(x, y), moves);
+                        break;
+                    case Knight.identifier:
+                        Knight.getPossibleMoves(state, new Square(x, y), moves);
+                        break;
+                    case King.identifier:
+                        King.getPossibleMoves(state, new Square(x, y), moves);
+                        break;
+                    case Queen.identifier:
+                        Queen.getPossibleMoves(state, new Square(x, y), moves);
+                        break;
+                }
+            }
+        }
+
+        return moves;
     }
 
 }
