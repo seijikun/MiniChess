6a8a66399a209b1e14f251d008144f40e042191f
Jonathan Ponzer <jonathan.ponzer@gmx.de>
2017-05-16 12:55:09 +0200

    Implemented RandomPlayer.getMove

--- src/li/seiji/minichess/player/RandomPlayer.java
+++ src/li/seiji/minichess/player/RandomPlayer.java
@@ -1,18 +1,18 @@
 package li.seiji.minichess.player;
 
+import li.seiji.minichess.Board;
 import li.seiji.minichess.State;
 import li.seiji.minichess.move.Move;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class RandomPlayer implements IPlayer {
     @Override
     public Move getMove(State state) {
-        //TODO: implement
-        return null;
-//        List<Move> currentMoves = new ArrayList<>();
-//        MoveGenerator.moveList(state, currentMoves, from);
-//
-//        int index = ThreadLocalRandom.current().nextInt(0, currentMoves.size()-1);
-//
-//        return currentMoves.get(index);
+        List<Move> currentMoves = Board.getPossibleMoves(state);
+
+        int index = ThreadLocalRandom.current().nextInt(0, currentMoves.size()-1);
+
+        return currentMoves.get(index);
     }
 }
